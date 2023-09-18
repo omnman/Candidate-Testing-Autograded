edc2432392ae21683eb3fa2b0b1d2838d2240f70
Andre Spencer <andrespencer@Andres-MacBook-Pro.local>
2023-09-17 22:02:40 -0500

    Part one

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,13 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+candidateName = ""
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+
+let question = ("Who was the first American woman in space? ");
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,19 +21,23 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("What is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question)
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer == "Sally Ride"){
+  console.log("That is correct!")
+} else {
+  console.log("You have answered incorrectly")
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +49,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Hello ${candidateName + "!"}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
