991b6d055b5bbe7cfbd22cfcd24b9e62445db28e
Andre <andrespencer33@yahoo.com>
2023-09-28 09:03:35 -0500

    Part2

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,9 +14,17 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  'Who was the first American woman in space? ', 
+'True or false: 5 kilometer == 5000 meters? ',
+'(5 + 3)/2 * 10 = ? ' ,
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " ,
+ 'What is the minimum crew size for the ISS? '
+]
+;
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3' 
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -26,22 +34,42 @@ candidateName = input.question("What is your name?")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
+let questionNumber = [0] 
+
+  while (questionNumber < questions.length){
+  candidateAnswers.push(input.question(questions[questionNumber]));
+questionNumber++
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == "Sally Ride"){
-  console.log("That is correct!")
+  let i = [0]
+
+while (i < correctAnswers.length) {
+if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+  console.log(`You answered ${candidateAnswers[i]} which is correct!`)
 } else {
   console.log("You have answered incorrectly")
 }
-
+i++
+}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+let numberOfCorrectAnswers = (Number())
+let numberOfQuestions = [0]
+  while (i < correctAnswers.length) {
+    numberOfQuestions++
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+      numberOfCorrectAnswers++
+      grade = numberOfCorrectAnswers/numberOfQuestions * 100
+    } else {
+      console.log(`You have answered ${questions[i]} incorrectly`)
+    }
+    i++
+    }
 
   return grade;
 }
